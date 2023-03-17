9de9c3a7861cd2fe2d19d5bef08ca1292b11b6cb
Jamey LaMar <102421586+SuperJamey@users.noreply.github.com>
2023-03-17 11:54:44 -0500

    use try/catch for T/S

--- controllers/register.js
+++ controllers/register.js
@@ -8,29 +8,34 @@ const handleRegister = (req, res, db, bcrypt) => {
     console.log('hash: ', hash);
     console.log('email: ', email);
     console.log('name: ', name);
-    db.transaction(trx => {
-      trx.insert({
-        hash: hash,
-        email: email
+    try {
+      db.transaction(trx => {
+        trx.insert({
+          hash: hash,
+          email: email
+        })
+        .into('users')
+        .returning('email')
+        .then(loginEmail => {
+          return trx('users')
+            .returning('*')
+            .insert({
+              email: loginEmail[0].email,
+              name: name,
+              joined: new Date()
+            })
+            .then(user => {
+              res.json(user[0]);s
+            })
+            .catch(console.log)
+        })
+      .then(trx.commit)
+      .catch(trx.rollback)
       })
-      .into('users')
-      .returning('email')
-      // .then(loginEmail => {
-      //   return trx('users')
-      //     .returning('*')
-      //     .insert({
-      //       email: loginEmail[0].email,
-      //       name: name,
-      //       joined: new Date()
-      //     })
-      //     .then(user => {
-      //       res.json(user[0]);s
-      //     })
-      //     .catch(console.log)
-      // })
-    // .then(trx.commit)
-    // .catch(trx.rollback)
-    })}
+    } catch(err) {
+      console.log(err)
+    }
+  }
 
 module.exports = {
   handleRegister: handleRegister
