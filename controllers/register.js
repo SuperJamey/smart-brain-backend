3ac93b8cde23eb2e4e993e4596c543b720d0fcf0
Jamey LaMar <102421586+SuperJamey@users.noreply.github.com>
2023-03-17 08:45:16 -0500

    test if code is making error

--- controllers/register.js
+++ controllers/register.js
@@ -1,38 +1,39 @@
-const handleRegister = (req, res, db, bcrypt) => {
-  console.log('in handleRegister');
-  const { email, name, password } = req.body;
-  if (!email || !name || !password) {
-    return res.status(400).json('incorrect form submission');
-  }
-  const hash = bcrypt.hashSync(password);
-    console.log('hash: ', hash);
-    console.log('email: ', email);
-    console.log('name: ', name);
-    return null
-    // db.transaction(trx => {
-    //   trx.insert({
-    //     hash: hash,
-    //     email: email
-    //   })
-    //   .into('login')
-    //   .returning('email')
-    //   .then(loginEmail => {
-    //     return trx('users')
-    //       .returning('*')
-    //       .insert({
-    //         email: loginEmail[0].email,
-    //         name: name,
-    //         joined: new Date()
-    //       })
-    //       .then(user => {
-    //         res.json(user[0]);s
-    //       })
-    //       .catch(console.log)
-    //   })
-    // .then(trx.commit)
-    // .catch(trx.rollback)
-    // })}
+// const handleRegister = (req, res, db, bcrypt) => {
+//   console.log('in handleRegister');
+//   const { email, name, password } = req.body;
+//   if (!email || !name || !password) {
+//     return res.status(400).json('incorrect form submission');
+//   }
+//   const hash = bcrypt.hashSync(password);
+//     console.log('hash: ', hash);
+//     console.log('email: ', email);
+//     console.log('name: ', name);
+//     db.transaction(trx => {
+//       trx.insert({
+//         hash: hash,
+//         email: email
+//       })
+//       .into('login')
+//       .returning('email')
+//       .then(loginEmail => {
+//         return trx('users')
+//           .returning('*')
+//           .insert({
+//             email: loginEmail[0].email,
+//             name: name,
+//             joined: new Date()
+//           })
+//           .then(user => {
+//             res.json(user[0]);s
+//           })
+//           .catch(console.log)
+//       })
+//     .then(trx.commit)
+//     .catch(trx.rollback)
+//     })}
 
-module.exports = {
-  handleRegister: handleRegister
-}}
+// module.exports = {
+//   handleRegister: handleRegister
+// }
+
+console.log('You made it to handleRegister. is there an errorrtre');
