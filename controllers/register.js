99fefb72eff041ce633b04c7e8da908fbbdd1a62
Jamey LaMar <102421586+SuperJamey@users.noreply.github.com>
2023-03-17 12:26:53 -0500

    T/S in process

--- controllers/register.js
+++ controllers/register.js
@@ -8,36 +8,30 @@ const handleRegister = (req, res, db, bcrypt) => {
     console.log('hash: ', hash);
     console.log('email: ', email);
     console.log('name: ', name);
-    try {
       db.transaction(trx => {
         trx.insert({
           hash: hash,
           email: email
+        })      
+        .into('users')      
+        .returning('email')      
+        .then(loginEmail => {
+          return trx('users')
+            .returning('*')
+            .insert({
+              email: loginEmail[0].email,
+              name: name,
+              joined: new Date()
+            })
+            .then(user => {
+              res.json(user[0]);s
+            })
+          .catch(console.log)
         })
-      
-        .into('users')
-      
-        //.returning('email')
-      
-        // .then(loginEmail => {
-        //   return trx('users')
-        //     .returning('*')
-        //     .insert({
-        //       email: loginEmail[0].email,
-        //       name: name,
-        //       joined: new Date()
-        //     })
-        //     .then(user => {
-        //       res.json(user[0]);s
-        //     })
-        //     .catch(console.log)
-        // })
-      .then(trx.commit)
-      .catch(trx.rollback)
-      })
-    } catch(err) {
-      console.log(err)
-    }
+        // .then(trx.commit)
+        // .catch(trx.rollback)
+        .catch(console.log)
+      });
   }
 
 module.exports = {
